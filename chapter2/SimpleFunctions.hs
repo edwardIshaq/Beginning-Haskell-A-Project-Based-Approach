fcb04aa7f398f51adc4eef3d6af2930b955c729e
Edward Ishaq <edward.ishaq@gmail.com>
2015-07-21 20:58:10 -0700

    exercise 5-2

--- chapter2/SimpleFunctions.hs
+++ chapter2/SimpleFunctions.hs
@@ -33,8 +33,33 @@ data Client = GovOrg String
 
 data Gender = Male | Female | Unknown deriving(Show)
 data Person = Person String String Gender deriving(Show)
+{--
+Time machines are defined by its
+manufacturer, its model (which is an integer),
+its name,
+whether they can travel to the past
+and to the future
+and a price (which can be represented as a floating-point number).
+--}
 data TimeMachine = TimeMachine String Integer String Bool Bool Float deriving(Show, Read)
 
+
+eddie = Person "Edward" "Ashak" Male
+shadi = Person "Shadi" "Shalabi" Male
+jamie = Person "Jamie" "Mandell" Female
+
+fbi = GovOrg "FBI"
+zuno  = Company "Zuno" 2973289 eddie "Future Interactions Strategist"
+booz  = Company "Booz Allen" 12039 jamie "Consoltant"
+dood  = Individual shadi False
+
+companies = [fbi, zuno, booz, dood]
+
+tm1 = TimeMachine "Apple" 32 "iTime" True False 100
+tm2 = TimeMachine "Apple" 35 "iTime 2" True False 200
+tm3 = TimeMachine "Apple" 39 "iTime 3" True False 300
+
+machines = [tm1, tm2, tm3]
 
 clientName :: Client -> String
 clientName client = case client of
@@ -42,4 +67,31 @@ clientName client = case client of
   Company name _ _ _-> name
   Individual (Person fname lname _) ads -> fname ++ " " ++ lname
 
-genderStats :: [Client] ->
+-- Exercise 2-5
+clientGender :: Client -> Gender
+clientGender (GovOrg _) = Unknown
+clientGender (Company _ _ (Person _ _ gender) _) = gender
+clientGender (Individual (Person _ _ gender) _ ) = gender
+
+{-For statistical purposes, write a function that returns the number of clients of each gender.
+You may need to define an auxiliary data type to hold the results of this function.-}
+genderState :: [Client] -> (Int, Int, Int) -- (Male, Female, Unknown)
+genderState [] = (0, 0, 0)
+genderState xs = (males, females, unknowns)
+  where genders = map clientGender xs
+        males = length $ [x | x@(Male) <- genders]
+        females = length $ [x | x@(Female) <- genders]
+        unknowns = length $ [x | x@(Unknown) <- genders]
+
+{-Every year a time comes when time machines are sold with a big discount to encourage potential buyers.
+Write a function that given a list of time machines, decreases their price by some percentage.
+Use the TimeMachine data type you defined in the previous set of exercises.-}
+timeMachineDiscount2 :: [TimeMachine] -> Float -> [TimeMachine]
+timeMachineDiscount2 [] _ = []
+timeMachineDiscount2 (x@(TimeMachine comp model name past future p):xs) discount = (TimeMachine comp model name past future (discount * p)) : (timeMachineDiscount2 xs discount)
+
+discount ::  Float -> TimeMachine -> TimeMachine
+discount rate (TimeMachine comp model name past future p)  = TimeMachine comp model name past future (rate * p)
+
+timeMachineDiscount :: [TimeMachine] -> Float -> [TimeMachine]
+timeMachineDiscount xs rate = map (discount rate) machines
