ae4a118f5c926c7747db354d4fedde6f515bad80
Edward Ishaq <edward.ishaq@gmail.com>
2015-07-24 16:16:34 -0700

    chapter 3 exercises

---
+++ chapter3/exercise.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE LambdaCase #-}
+swapTriple :: (a,b,c) -> (b,c,a)
+swapTriple (x,y,z) = (y,z,x)
+
+duplicate :: a -> (a,a)
+duplicate x = (x,x)
+
+nothing :: t -> Maybe a
+nothing _ = Nothing
+
+-- index :: [a] -> [(Num,a)]
+index :: Num t => [a] -> [(t, a)]
+index []     = []
+index [x]    = [(0,x)]
+index (x:xs) = let indexed@((n,_):_) = index xs
+                                      in  (n+1,x):indexed
+
+maybeA :: [t] -> Char
+maybeA [] = 'a'
+
+
+sayHello :: [String] -> [String]
+-- sayHello names = map (\name -> case name of
+--                                  "Alejandro" -> "Hello, writer"
+--                                  _           -> "Welcome, " ++ name
+--                      ) names
+
+-- Using LambdaCase
+sayHello names = map (\case "Alejandro" -> "Hello, writer"
+                            name        -> "Welcome, " ++ name
+                    ) names
+
+{-
+Using the function filter as the basis for your solution, write the following functions:
+filterOnes, which returns only the elements equal to the constant 1.
+filterANumber, which returns only the elements equal to some number which is given via a parameter.
+filterNot, which performs the reverse duty of filter: it returns only those elements of the list which do not fulfill the condition.
+filterGovOrgs, which takes a list of Clients (as defined before) and returns only those which are government organizations.
+Write it using both an auxiliary function isGovOrg and a \case expression.
+-}
+filterOnes :: (Num a, Eq a) => [a] -> [a]
+filterOnes xs = filter (\x -> x == 1) xs
+
+filterANumber :: (Num a, Eq a) => a -> [a] -> [a]
+filterANumber n xs = filter (\x -> x == n) xs
+
+filterNot :: (a -> Bool) -> [a] -> [a]
+filterNot f xs = filter (not . f) xs
